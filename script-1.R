b73509cafb9a18f43e94ff358634f9bab65cbca3
rameenf1024 <rameenf1024@gmail.com>
2023-04-14 23:21:37 +0500

    My first file uploaded

---
+++ script-1.R
@@ -0,0 +1,13 @@
+print(7+7)
+
+
+s<-"soup"
+typeof(s)
+
+library
+
+# give access to repositories/activates repositories
+setRepositories()
+install.packages("WriteXLS", dependencies = TRUE)
+
+library(WriteXLS)
